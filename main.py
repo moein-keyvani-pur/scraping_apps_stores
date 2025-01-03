50942070bf3fb11497d38512ab7a432f810a9e1d
Moein Keyvanipour <81565043+moein-keyvani-pur@users.noreply.github.com>
2025-01-03 22:36:10 +0330

    run githu action v1.5

--- main.py
+++ main.py
@@ -73,13 +73,13 @@ if __name__ == "__main__":
 
     create_csv(csv_file_path, header)
 
-    app_url = f"{cts.base_rul}{cts.app_path}"
-    apps_category = cts.apps_category[7:]  #! change 2 
-    get_apps_games_myket(app_url, apps_category)
+    # app_url = f"{cts.base_rul}{cts.app_path}"
+    # apps_category = cts.apps_category[7:]  #! change 2 
+    # get_apps_games_myket(app_url, apps_category)
 
-    # games_url = f"{cts.base_rul}{cts.game_path}"
-    # games_category = cts.game_category
-    # get_apps_games_myket(games_url, games_category)
+    games_url = f"{cts.base_rul}{cts.game_path}"
+    games_category = cts.game_category
+    get_apps_games_myket(games_url, games_category)
 
     driver.close()
     driver.quit()
